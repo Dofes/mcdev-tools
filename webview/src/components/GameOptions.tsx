abc12f13eaf469624d7790b0f5c626c342fbee01
Github-Zero123 <mcwitherboss@outlook.com>
2026-05-15 12:45:59 +0800

    feat: 添加复选框网格布局，优化选项显示

--- webview/src/components/GameOptions.tsx
+++ webview/src/components/GameOptions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import type { FC } from 'react';
 import { I18nText } from '../i18n';
 import { useDefaultValues } from '../hooks/useDefaultValues';
 
@@ -20,7 +20,6 @@ interface Props {
   markInitialized?: (componentId: string) => void;
 }
 
-
 const DEFAULT_VALUES: McdevData = {
   reset_world: false,
   auto_join_game: true,
@@ -32,8 +31,18 @@ const DEFAULT_VALUES: McdevData = {
   do_weather_cycle: true,
 };
 
-export const GameOptions: React.FC<Props> = ({ t, data, onDataChange, markInitialized }) => {
-  useDefaultValues(data, DEFAULT_VALUES, onDataChange, markInitialized ? () => markInitialized('GameOptions') : undefined);
+export const GameOptions: FC<Props> = ({
+  t,
+  data,
+  onDataChange,
+  markInitialized,
+}) => {
+  useDefaultValues(
+    data,
+    DEFAULT_VALUES,
+    onDataChange,
+    markInitialized ? () => markInitialized('GameOptions') : undefined,
+  );
 
   return (
     <>
@@ -45,44 +54,60 @@ export const GameOptions: React.FC<Props> = ({ t, data, onDataChange, markInitia
           </span>
         </div>
 
-        <div className="checkbox-group">
-          <input
-            type="checkbox"
-            id="reset_world"
-            checked={data.reset_world ?? DEFAULT_VALUES.reset_world}
-            onChange={(e) => onDataChange('reset_world', e.target.checked)}
-          />
-          <label htmlFor="reset_world">{t.resetWorld}</label>
-        </div>
+        <div className="checkbox-grid">
+          <div className="checkbox-group">
+            <input
+              type="checkbox"
+              id="reset_world"
+              checked={data.reset_world ?? DEFAULT_VALUES.reset_world}
+              onChange={(e) => onDataChange('reset_world', e.target.checked)}
+            />
+            <label htmlFor="reset_world" title={t.resetWorld}>
+              {t.resetWorld}
+            </label>
+          </div>
 
-        <div className="checkbox-group">
-          <input
-            type="checkbox"
-            id="auto_join_game"
-            checked={data.auto_join_game ?? DEFAULT_VALUES.auto_join_game}
-            onChange={(e) => onDataChange('auto_join_game', e.target.checked)}
-          />
-          <label htmlFor="auto_join_game">{t.autoJoin}</label>
-        </div>
+          <div className="checkbox-group">
+            <input
+              type="checkbox"
+              id="auto_join_game"
+              checked={data.auto_join_game ?? DEFAULT_VALUES.auto_join_game}
+              onChange={(e) => onDataChange('auto_join_game', e.target.checked)}
+            />
+            <label htmlFor="auto_join_game" title={t.autoJoin}>
+              {t.autoJoin}
+            </label>
+          </div>
 
-        <div className="checkbox-group">
-          <input
-            type="checkbox"
-            id="include_debug_mod"
-            checked={data.include_debug_mod ?? DEFAULT_VALUES.include_debug_mod}
-            onChange={(e) => onDataChange('include_debug_mod', e.target.checked)}
-          />
-          <label htmlFor="include_debug_mod">{t.includeDebug}</label>
-        </div>
+          <div className="checkbox-group">
+            <input
+              type="checkbox"
+              id="include_debug_mod"
+              checked={data.include_debug_mod ?? DEFAULT_VALUES.include_debug_mod}
+              onChange={(e) =>
+                onDataChange('include_debug_mod', e.target.checked)
+              }
+            />
+            <label htmlFor="include_debug_mod" title={t.includeDebug}>
+              {t.includeDebug}
+            </label>
+          </div>
 
-        <div className="checkbox-group">
-          <input
-            type="checkbox"
-            id="auto_hot_reload_mods"
-            checked={data.auto_hot_reload_mods ?? DEFAULT_VALUES.auto_hot_reload_mods}
-            onChange={(e) => onDataChange('auto_hot_reload_mods', e.target.checked)}
-          />
-          <label htmlFor="auto_hot_reload_mods">{t.autoHotReload}</label>
+          <div className="checkbox-group">
+            <input
+              type="checkbox"
+              id="auto_hot_reload_mods"
+              checked={
+                data.auto_hot_reload_mods ?? DEFAULT_VALUES.auto_hot_reload_mods
+              }
+              onChange={(e) =>
+                onDataChange('auto_hot_reload_mods', e.target.checked)
+              }
+            />
+            <label htmlFor="auto_hot_reload_mods" title={t.autoHotReload}>
+              {t.autoHotReload}
+            </label>
+          </div>
         </div>
       </div>
 
@@ -94,46 +119,62 @@ export const GameOptions: React.FC<Props> = ({ t, data, onDataChange, markInitia
           </span>
         </div>
 
-        <div className="checkbox-group">
-          <input
-            type="checkbox"
-            id="enable_cheats"
-            checked={data.enable_cheats ?? DEFAULT_VALUES.enable_cheats}
-            onChange={(e) => onDataChange('enable_cheats', e.target.checked)}
-          />
-          <label htmlFor="enable_cheats">{t.enableCheats}</label>
+        <div className="checkbox-grid">
+          <div className="checkbox-group">
+            <input
+              type="checkbox"
+              id="enable_cheats"
+              checked={data.enable_cheats ?? DEFAULT_VALUES.enable_cheats}
+              onChange={(e) => onDataChange('enable_cheats', e.target.checked)}
+            />
+            <label htmlFor="enable_cheats" title={t.enableCheats}>
+              {t.enableCheats}
+            </label>
+          </div>
+
+          <div className="checkbox-group">
+            <input
+              type="checkbox"
+              id="keep_inventory"
+              checked={data.keep_inventory ?? DEFAULT_VALUES.keep_inventory}
+              onChange={(e) => onDataChange('keep_inventory', e.target.checked)}
+            />
+            <label htmlFor="keep_inventory" title={t.keepInventory}>
+              {t.keepInventory}
+            </label>
+          </div>
+
+          <div className="checkbox-group">
+            <input
+              type="checkbox"
+              id="do_daylight_cycle"
+              checked={
+                data.do_daylight_cycle ?? DEFAULT_VALUES.do_daylight_cycle
+              }
+              onChange={(e) =>
+                onDataChange('do_daylight_cycle', e.target.checked)
+              }
+            />
+            <label htmlFor="do_daylight_cycle" title={t.doDaylightCycle}>
+              {t.doDaylightCycle}
+            </label>
+          </div>
+
+          <div className="checkbox-group">
+            <input
+              type="checkbox"
+              id="do_weather_cycle"
+              checked={data.do_weather_cycle ?? DEFAULT_VALUES.do_weather_cycle}
+              onChange={(e) =>
+                onDataChange('do_weather_cycle', e.target.checked)
+              }
+            />
+            <label htmlFor="do_weather_cycle" title={t.doWeatherCycle}>
+              {t.doWeatherCycle}
+            </label>
+          </div>
         </div>
-
-      <div className="checkbox-group">
-        <input
-          type="checkbox"
-          id="keep_inventory"
-          checked={data.keep_inventory ?? DEFAULT_VALUES.keep_inventory}
-          onChange={(e) => onDataChange('keep_inventory', e.target.checked)}
-        />
-        <label htmlFor="keep_inventory">{t.keepInventory}</label>
       </div>
-
-      <div className="checkbox-group">
-        <input
-          type="checkbox"
-          id="do_daylight_cycle"
-          checked={data.do_daylight_cycle ?? DEFAULT_VALUES.do_daylight_cycle}
-          onChange={(e) => onDataChange('do_daylight_cycle', e.target.checked)}
-        />
-        <label htmlFor="do_daylight_cycle">{t.doDaylightCycle}</label>
-      </div>
-
-      <div className="checkbox-group">
-        <input
-          type="checkbox"
-          id="do_weather_cycle"
-          checked={data.do_weather_cycle ?? DEFAULT_VALUES.do_weather_cycle}
-          onChange={(e) => onDataChange('do_weather_cycle', e.target.checked)}
-        />
-        <label htmlFor="do_weather_cycle">{t.doWeatherCycle}</label>
-      </div>
-    </div>
     </>
   );
 };
